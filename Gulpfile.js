6f21cf2536f44b7ffbb108348257bbf3b204563d
andrew <andrewxhill@gmail.com>
2018-06-22 16:51:10 -0700

    adds invite mechanisms for deep-link misses

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,12 +3,28 @@ var sass = require('gulp-sass')
 var useref = require('gulp-useref')
 var uglify = require('gulp-uglify')
 var gulpIf = require('gulp-if')
+var ext_replace = require('gulp-ext-replace')
+var rename = require("gulp-rename")
 var cssnano = require('gulp-cssnano')
 var cache = require('gulp-cache')
 var imagemin = require('gulp-imagemin')
 var browserSync = require('browser-sync').create()
+var reload      = browserSync.reload;
 var runSequence = require('run-sequence')
 var del = require('del')
+
+gulp.task('serve', function() {
+  browserSync.init({
+    port: "8000",
+    server: {
+      baseDir: "./src",
+      serveStaticOptions: {
+        extensions: ['html']
+      }
+    }
+  });
+  gulp.watch(['src/**/*.html', 'src/**/*.js', 'src/**/*.css']).on("change", reload);
+})
 
 gulp.task('clean:dist', function() {
   return del.sync('dist');
@@ -26,11 +42,14 @@ gulp.task('default',function() {
 })
 
 gulp.task('useref', function(){
-  return gulp.src('src/**/*.+(html|js|css|json)')
+  return gulp.src(['src/**/*.+(html|js|css|json)'])
       .pipe(useref())
       .pipe(gulpIf('*.js', uglify()))
       // Minifies only if it's a CSS file
       .pipe(gulpIf('*.css', cssnano()))
+      .pipe(gulpIf('**/new.html', rename(function(file) {
+        file.extname = '';
+      })))
       .pipe(gulp.dest('dist'))
 });
 
